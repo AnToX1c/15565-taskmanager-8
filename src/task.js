b784299757b50e10c32090a16876835a0fb76a9c
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-03-09 11:58:08 +0200

    Create new components Task & TaskEdit / Complete module4-task1

--- src/task.js
+++ src/task.js
@@ -1,21 +1,39 @@
-const renderCardElement = (task) => {
-  const generateHashtags = (tags) => tags.length > 0 ? tags.map((it) => `
-                        <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <button type="button" class="card__hashtag-name">
-                            #${it}
-                            </button>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>`).join(``) : ``;
-  return `<article class="card card--blue">
-            <form class="card__form" method="get">
+import createElement from './create-element.js';
+
+export default class Task {
+  constructor(data) {
+    this._title = data.title;
+    this._dueDate = data.dueDate;
+    this._tags = data.tags;
+    this._picture = data.picture;
+    this._color = data.color;
+    this._repeatingDays = data.repeatingDays;
+
+    this._element = null;
+    this._state = {
+    };
+
+    this._onEdit = null;
+  }
+
+  _isRepeated() {
+    return Object.values(this._repeatingDays).some((it) => it === true);
+  }
+
+  _onEditButtonClick() {
+    typeof this._onEdit === `function` && this._onEdit();
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  set onEdit(fn) {
+    this._onEdit = fn;
+  }
+
+  get template() {
+    return `<article class="card card--blue ${this._isRepeated() ? `card--repeat` : ``}">
               <div class="card__inner">
                 <div class="card__control">
                   <button type="button" class="card__btn card__btn--edit">
@@ -44,7 +62,7 @@ const renderCardElement = (task) => {
                       class="card__text"
                       placeholder="Start typing your text here..."
                       name="text"
-                    >${task.title}</textarea>
+                    >${this._title}</textarea>
                   </label>
                 </div>
 
@@ -161,7 +179,12 @@ const renderCardElement = (task) => {
 
                     <div class="card__hashtag">
                       <div class="card__hashtag-list">
-                        ${generateHashtags(task.tags)}
+                        ${(Array.from(this._tags).map((tag) => (`
+                          <span class="card__hashtag-inner">
+                            <input type="hidden" name="hashtag" value="${tag}" class="card__hashtag-hidden-input" />
+                            <button type="button" class="card__hashtag-name">#${tag}</button>
+                            <button type="button" class="card__hashtag-delete">delete</button>
+                          </span>`.trim()))).join(``)}
                       </div>
 
                       <label>
@@ -182,88 +205,32 @@ const renderCardElement = (task) => {
                       name="img"
                     />
                     <img
-                      src="${task.picture}"
+                      src="${this._picture}"
                       alt="task picture"
                       class="card__img"
                     />
                   </label>
-
-                  <div class="card__colors-inner">
-                    <h3 class="card__colors-title">Color</h3>
-                    <div class="card__colors-wrap">
-                      <input
-                        type="radio"
-                        id="color-black-6"
-                        class="card__color-input card__color-input--black visually-hidden"
-                        name="color"
-                        value="black"
-                      />
-                      <label
-                        for="color-black-6"
-                        class="card__color card__color--black"
-                        >black</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-yellow-6"
-                        class="card__color-input card__color-input--yellow visually-hidden"
-                        name="color"
-                        value="yellow"
-                      />
-                      <label
-                        for="color-yellow-6"
-                        class="card__color card__color--yellow"
-                        >yellow</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-blue-6"
-                        class="card__color-input card__color-input--blue visually-hidden"
-                        name="color"
-                        value="blue"
-                      />
-                      <label
-                        for="color-blue-6"
-                        class="card__color card__color--blue"
-                        >blue</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-green-6"
-                        class="card__color-input card__color-input--green visually-hidden"
-                        name="color"
-                        value="green"
-                        checked
-                      />
-                      <label
-                        for="color-green-6"
-                        class="card__color card__color--green"
-                        >green</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-pink-6"
-                        class="card__color-input card__color-input--pink visually-hidden"
-                        name="color"
-                        value="pink"
-                      />
-                      <label
-                        for="color-pink-6"
-                        class="card__color card__color--pink"
-                        >pink</label
-                      >
-                    </div>
-                  </div>
                 </div>
+          </article>`.trim();
+  }
 
-                <div class="card__status-btns">
-                  <button class="card__save" type="submit">save</button>
-                  <button class="card__delete" type="button">delete</button>
-                </div>
-              </div>
-            </form>
-          </article>
-  `;
-};
+  bind() {
+    this._element.querySelector(`.card__btn--edit`)
+      .addEventListener(`click`, this._onEditButtonClick.bind(this));
+  }
 
-export default renderCardElement;
+  unbind() {
+    // Удаление обработчиков
+  }
+
+  render() {
+    this._element = createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+
+  unrender() {
+    this.unbind();
+    this._element = null;
+  }
+}
